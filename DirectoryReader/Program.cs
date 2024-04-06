ba7024b5c09a20d481a2ad831844a54e1bb68364
Joelbu <89338010+Joelbu537@users.noreply.github.com>
2024-04-07 01:13:00 +0200

    update program.cs

--- DirectoryReader/Program.cs
+++ DirectoryReader/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System.Diagnostics;
+using System.Runtime.Intrinsics.Arm;
 using System.Security.Principal;
+using System.Text;
 using static System.Net.Mime.MediaTypeNames;
+using System.Security.Cryptography;
 
 class Program
 {
@@ -13,6 +16,7 @@ class Program
         bool alive = true;
         while(alive)
         {
+            Console.Clear();
             try //BUG HUNTING
             {
                 WriteCurrent();
@@ -23,10 +27,28 @@ class Program
                 ThrowError(ex.Message);
                 Console.ReadKey();
             }
+            Console.Clear();
         }
     }
     static void WriteCurrent()
     {
+        Console.Clear();
+        if(currentpath.Length == 2)
+        {
+            currentpath += "\\";
+            if(Directory.Exists(currentpath))
+            {
+                workingpath = currentpath;
+            }
+            else
+            {
+                currentpath = workingpath;
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("[!GHOST DIRECTORY!]");
+                Console.WriteLine("RESTART PROGRAM");
+                Console.ReadKey();
+            }
+        }
         Console.Clear();
         Console.ForegroundColor = ConsoleColor.Black;
         Console.BackgroundColor = ConsoleColor.White;
@@ -51,27 +73,41 @@ class Program
         Console.WriteLine(currentpath);
         string[] filesInDirectory = Directory.GetFiles(currentpath);
         string[] foldersInDirectory = Directory.GetDirectories(currentpath);
-        Console.ForegroundColor = ConsoleColor.Black;
-        Console.BackgroundColor = ConsoleColor.White;
-        Console.Write("[FILES]    ");
-        Console.ResetColor();
-        Console.WriteLine($" {filesInDirectory.Length + foldersInDirectory.Length} ({filesInDirectory.Length}/{foldersInDirectory.Length})");
         Console.WriteLine();
-        foreach(string folder in foldersInDirectory)
+        if(filesInDirectory.Length == 0 && foldersInDirectory.Length == 0)
         {
-            string folderName = new DirectoryInfo(folder).Name;
-            Console.WriteLine($"{folderName}\\..");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("[!DIRECTORY EMPTY OR CORRUPTED!]");
         }
-        foreach (string file in filesInDirectory)
+        else
         {
-            string filename = Path.GetFileName(file);
-            Console.WriteLine($"{filename}");
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Write("[FOLDERS]  ");
+            Console.ResetColor();
+            Console.WriteLine($" {foldersInDirectory.Length}");
+            foreach (string folder in foldersInDirectory)
+            {
+                string folderName = new DirectoryInfo(folder).Name;
+                Console.WriteLine($"{folderName}\\..");
+            }
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Write("[FILES]    ");
+            Console.ResetColor();
+            Console.WriteLine($" {filesInDirectory.Length}");
+            foreach (string file in filesInDirectory)
+            {
+                string filename = Path.GetFileName(file);
+                Console.WriteLine($"{filename}");
+            }
+            Console.WriteLine();
+            string firstPart = Path.GetPathRoot(currentpath);
+            string lastPart = Path.GetFileName(currentpath);
+            string newFilePath = Path.Combine(firstPart, "...", lastPart);
+            Console.Write(newFilePath + "\\");
         }
-        Console.WriteLine();
-        string firstPart = Path.GetPathRoot(currentpath);
-        string lastPart = Path.GetFileName(currentpath);
-        string newFilePath = Path.Combine(firstPart,"...", lastPart);
-        Console.Write(newFilePath + "\\");
     }
     static void HandleInput(string input)
     {
@@ -81,14 +117,23 @@ class Program
             case "cd":
                 if (inputs[1] == "..")
                 {
-                    currentpath = currentpath.Substring(0, currentpath.LastIndexOf('\\'));
-                    if (Directory.Exists(currentpath))
+                    if(currentpath.Length > 4)
                     {
-                        workingpath = currentpath;
+                        currentpath = currentpath.Substring(0, currentpath.LastIndexOf('\\'));
+                        if (Directory.Exists(currentpath))
+                        {
+                            workingpath = currentpath;
+                        }
+                        else
+                        {
+                            currentpath = workingpath;
+                        }
                     }
                     else
                     {
-                        currentpath = workingpath;
+                        ThrowError("Invalid path!");
+                        Console.WriteLine("Press any key to continue");
+                        Console.ReadKey();
                     }
                 }
                 else
@@ -159,6 +204,41 @@ class Program
                     Console.ReadKey();
                 }
                 break;
+            case "encrypt":
+                Console.Clear();
+                string[] filesInDirectory = Directory.GetFiles(currentpath);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"{filesInDirectory.Length} files will be encrypted!");
+                Console.ResetColor();
+                string entertext = "Enter password to confirm: ";
+                string password = CheckPassword(entertext);
+                string hash = getHashSha256(password);
+                password = null;
+                GC.Collect();
+                if(hash == "49f74582bd0a7b97b806e65bd529fbcedaff4b3bdf01b1fd31c63769c8a050d0")
+                {
+                    for(int i = 10; i > -1; i--)
+                    {
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Password correct!");
+                        Console.ResetColor();
+                        Console.WriteLine($"Encrypting files in {i} seconds!");
+                        Thread.Sleep(1000);
+                    }
+                    Console.Clear();
+                    //BAD STUFF HAPPENS HERE :O
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Password incorrect!");
+                    Console.ReadKey();
+                    Console.ReadKey();
+                }
+                
+                break;
             default:
                 Console.ForegroundColor = ConsoleColor.Red;
                 ThrowError("Command unknown. Check if there are any spelling mistakes.");
@@ -166,11 +246,72 @@ class Program
                 Console.ReadKey();
                 break;
         }
+        Console.Clear();
     }
     static void ThrowError(string error)
     {
         Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine($"Error: {error}");
         Console.ResetColor();
+    }
+    static string CheckPassword(string EnterText)
+    {
+        string EnteredVal = "";
+        try
+        {
+            Console.Write(EnterText);
+            EnteredVal = "";
+            do
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+                if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
+                {
+                    EnteredVal += key.KeyChar;
+                    Console.Write("*");
+                }
+                else
+                {
+                    if (key.Key == ConsoleKey.Backspace && EnteredVal.Length > 0)
+                    {
+                        EnteredVal = EnteredVal.Substring(0, (EnteredVal.Length - 1));
+                        Console.Write("\b \b");
+                    }
+                    else if (key.Key == ConsoleKey.Enter)
+                    {
+                        if (string.IsNullOrWhiteSpace(EnteredVal))
+                        {
+                            Console.WriteLine("");
+                            Console.WriteLine("Empty value not allowed.");
+                            CheckPassword(EnterText);
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("");
+                            break;
+                        }
+                    }
+                }
+            } while (true);
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+        return EnteredVal;
+    }
+    public static string getHashSha256(string text)
+    {
+        string salt = "jimmy";
+        text = salt + text;
+        byte[] bytes = Encoding.Unicode.GetBytes(text);
+        SHA256Managed hashstring = new SHA256Managed();
+        byte[] hash = hashstring.ComputeHash(bytes);
+        string hashString = string.Empty;
+        foreach (byte x in hash)
+        {
+            hashString += String.Format("{0:x2}", x);
+        }
+        return hashString;
     }
 }
